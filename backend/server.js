af77f6a6a9dfec081fee3153f07455a2e8e0c952
FouzanPS <mohammedfouzanmsd7@gmail.com>
2024-09-23 11:26:36 +0530

    update2

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,9 @@ app.use(cors());
 app.use(express.json()); 
 app.use('/generate', questionsRouter);
 
+app.get('/', (req, res) => {
+    res.send("Server running")
+})
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
